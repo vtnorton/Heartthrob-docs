8dc3c284bd401790133fb2d76938911e895e5b72
Vítor Norton <vtnorton@outlook.com>
2018-09-17 23:25:44 -0300

    Novos tipos de botões

--- Heartthrob/wwwroot/js/site.js
+++ Heartthrob/wwwroot/js/site.js
@@ -40,11 +40,12 @@
 
     var modal = document.querySelector(".alert-modal");
     dialog = function (env) {
-        var html = `<div id='dialog52895' class="alert"><h3>` + env.title + `</h3><p>` + env.description + `</p>
+        /*var html = `<div id='dialog52895' class="alert"><h3>` + env.title + `</h3><p>` + env.description + `</p>
                 <a class="btn close" href="#">` + env.cancel + `</a> <a class="btn btn-primary" onclick='` + env.action + `'>` + env.confirm + `</a></div>`;
+                */
 
-        alert(env.title);
-        //modal.classList.toggle("show-alert");
+        //alert(env.title);
+        modal.classList.toggle("show-alert");
     }
     closeDialog = function () {
         modal.classList.toggle("show-alert");
